f870ebb44694962ede3141fd9cf68e4ee71519dd
Vafa Sadri <vafasadrif12@gmail.com>
2023-08-14 12:32:32 +0330

    documentaion

--- SieveAlg/Header.hpp
+++ SieveAlg/Header.hpp
@@ -15,12 +15,26 @@ extern "C" {
 }
 using namespace std;
 using unit = unsigned long long;
+/// <summary>
+/// This struct is passed to each thread at its starting point
+/// </summary>
 struct WorkerMetadata {
+	/// <summary>
+	///  The initial number the thread has to start its first buffer with
+	/// <para>This works somehow like an index, it indicates which buffer ranges are for this thread</para>
+	/// </summary>
 	unit start;
+	/// <summary>
+	/// A pointer to the initial prime numbers
+	/// </summary>
 	unit* primes;
+	
+	/// <summary>
+	/// the last prime of each segment, meant to be printed for decoration
+	/// </summary>
 	volatile unit* currentResult;
 };
 constexpr size_t WorkerCount = 3;
-constexpr size_t BufferSize = 1 << 20;
+constexpr size_t SegmentLength = 1 << 20; /* 1MB segments */
 volatile unit ResultList[WorkerCount]{};
 std::array<std::thread*, WorkerCount> Workers;
--- SieveAlg/SieveAlg.cpp
+++ SieveAlg/SieveAlg.cpp
@@ -3,12 +3,13 @@
 #include "Header.hpp"
 // this is a simple sieve algorithm giving us around 1 million prime numbers to start with
 std::vector<unit> SimpleSieve(unit limit) {
+	// turn limit into the closest power of 2
 	{
 		unit ts = 4;
 		while (ts < limit)
 		{
 			ts *= 2;
-		}
+		}	
 		limit = ts;
 	}
 	std::vector<unit> primeBuff = {};
@@ -28,7 +29,7 @@ std::vector<unit> SimpleSieve(unit limit) {
 		}
 	}
 	unit i = sq;
-	// make sure is odd
+	// make sure 'i' is odd
 	if ((i & 1) == 0) i++;	
 	for (; i < limit; i +=2)
 	{		
@@ -41,24 +42,28 @@ std::vector<unit> SimpleSieve(unit limit) {
 }
 
 void SegmentedSieve(WorkerMetadata params) {
-	if (BufferSize % 8) throw std::exception("buffer size is not aligned");
-	unit bufferStart = params.start;
-	unit* primes = params.primes;
+	if (SegmentLength % 8) throw std::exception("buffer size is not aligned");
+	const unit* primes = params.primes;
+	/// <summary>
+	/// the number the zero'th index of buffer represents
+	/// <para>it always follows this formula :   </para>
+	/// <para> WorkerCount * SegmentLength * {CyclesPassed} + params.start</para>
+	/// </summary>
+	unit SegmentStart = params.start;
 	auto q = params.currentResult;
 	// an array of bools, each bool indicates whether BufferStart + {its index} is prime or not
-	// bitset and vector<bool> take up less space but they're a lot slower
+	// bitset and vector<bool> take up 8 times less space but they're a lot slower
 	// so forget about changing this	 
-	bool* const buffer = new bool[BufferSize];	
+	bool* const buffer = new bool[SegmentLength];	
 
 	while (true)
 	{
 		// UPDATE: I've decided not to waste any more resources on marking the even numbers
 		// since i can simply ignore them when iterating through
-		// UPDATE 2: this loop initialized the buffer so it doesn't contain junk values
-		// as well as marking the even numbers, so it can't be removed
+		// UPDATE 2: this loop initialized the buffer as well, so it can't be removed
 		{
 			unsigned long long* longbuf = (unsigned long long*)buffer;
-			unsigned long long* longend = (unsigned long long*)(buffer + BufferSize);
+			unsigned long long* longend = (unsigned long long*)(buffer + SegmentLength);
 			while (longbuf < longend)
 			{
 				*longbuf = 0x0100010001000100;
@@ -67,7 +72,7 @@ void SegmentedSieve(WorkerMetadata params) {
 		}
 		
 		// we'll first assume that all numbers are prime, and then multiply different numbers and mark the product as a compound number	
-		unit bufferEnd = bufferStart + BufferSize; 	// the number end of the buffer indicates
+		unit bufferEnd = SegmentStart + SegmentLength; 	// the number end of the buffer indicates
 		unit endSqrt = ceil(sqrt(bufferEnd));
 		// we'll only use prime numbers as p because if it was compound number, 4 for example, every product of it would also be a product of 2
 		// and already marked when iterating with p = 2		
@@ -75,38 +80,32 @@ void SegmentedSieve(WorkerMetadata params) {
 		for (const unit* i = primes; *i < endSqrt; i++)
 		{
 			unit p = *i;
-			auto div =  std::lldiv(bufferStart,p);
-		
+			auto div = std::lldiv(SegmentStart, p);
+			// the smallest multiplier for p where m * p >= bufferStart (other words: its within buffer range)
+			// the minimum for m is p because multipliers smaller than p
+			// would create duplicate products and reduce performance
+			// for example p = 11 , m = 2 -> p * m = 22
+			// and also p = 2 , m = 11 -> p * m = 22
+			// and thats a duplicate
 			unit m = std::max(p,static_cast<unit>(div.quot) + (div.rem != 0));		
 			if ((m % 2) == 0) m++;
-			bool* j = buffer + (p * m) - bufferStart;
-			bool* bEnd = buffer + BufferSize;
+			bool* j = buffer + (p * m) - SegmentStart;
+			bool* bEnd = buffer + SegmentLength;
 
 			while (j < bEnd)
 			{
 				*j = false;
 				j += 2 * p;
 			}			
-		}
-		
-	
-	
-		// the smallest multiplier for p where m * p >= bufferStart (other words its within buffer range)
-		// the minimum for m is p because multipliers smaller than p
-		//  would create duplicate compounds and slow down the program
-		// for example p = 11 , m = 2 => p * m = 22
-		// and also p = 2 , m = 11 => p * m = 22
-		// and thats a duplicate
-		
+		}			
 		// now all the compound numbers are marked with 'false' and the primes are left 'true'	
-		size_t i = BufferSize - 1;
+		size_t i = SegmentLength - 1;
 		while (!buffer[i])
 		{
 			i--;
 		}
-		*q = bufferStart + i;
-
-		bufferStart += WorkerCount * BufferSize;
+		*params.currentResult = SegmentStart + i;
+		SegmentStart += WorkerCount * SegmentLength;
 	}
 }
 // this function formats the numbers we want to print
@@ -115,32 +114,34 @@ void SegmentedSieve(WorkerMetadata params) {
 // 2100900 -> 2,100,900
 // I know this is not the most efficient way possible
 // but the performance doesn't really depend on this function
-void format(string& x) {
-	for (long long i = x.size() - 3; i > 0; i -= 3)
+void format(string& str) {
+	for (long long i = str.size() - 3; i > 0; i -= 3)
 	{
-		x.insert(x.begin() + i, ',');
+		str.insert(str.begin() + i, ',');
 	}
 }
 int main()
 { 
 	size_t x = 0;
 	auto primes = SimpleSieve(1ll << 24);	
+	// starting the workers
 	for (size_t i = 0; i < WorkerCount; i++)
 	{
 		Workers[i] = new thread(
 			SegmentedSieve,
 			WorkerMetadata{
-				static_cast<unit>(i * BufferSize),
+				static_cast<unit>(i * SegmentLength),
 				primes.data(),
-				&ResultList[i] 
+				&ResultList[i]
 			}
-		);	
+		);
 		string s = "Worker " + to_string(i + 1) + ": ";
 		x = std::max(s.length(), x);
 		cout << s << endl;
 	}
 	cout.flush();
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);	
+	// printing the results to console
 	while (true) {
 		COORD cord = { x,0 };
 
